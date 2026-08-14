a9968299835483c44d3c962b0c674502237cd507
Rajmahiii21 <rajm93415@gmail.com>
2026-08-14 22:00:03 +0530

    Fix controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,7 +6,15 @@ function tokenFor(user) {
   return jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "7d" });
 }
 function setCookie(res, token) {
-  res.cookie("token", token, { httpOnly: true, sameSite: "lax", secure: process.env.NODE_ENV === "production", maxAge: 7*24*60*60*1000 });
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite:
+        process.env.NODE_ENV === "production"
+            ? "none"
+            : "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+});
 }
 export async function register(req,res,next){
   try {
